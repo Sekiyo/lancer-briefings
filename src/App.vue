6e19c6e7a7af0d9e2ec48d5e90ea8ea11b48b201
Sekiyo <33166234+Sekiyo@users.noreply.github.com>
2022-04-13 01:32:52 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -125,12 +125,12 @@ export default {
           "mech": "The Final Act of Time and Reason"
         },
         {
-          "callsign": "Badlands",
-          "alias": "Marlene Gatsby",
+          "callsign": "Frontliner",
+          "alias": "Rico Burges",
           "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Mayfly"
+          "mech": "Stupid Robot"
         },
         {
           "callsign": "Compadre",
